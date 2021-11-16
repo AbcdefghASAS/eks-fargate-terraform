ae6ef39fd4c63a83dfbeb2890ce850ea326d3126
Akshaydwagh <49894158+Akshaydwagh@users.noreply.github.com>
2021-11-16 21:30:12 +0530

    Update variables.tf

--- network/variables.tf
+++ network/variables.tf
@@ -1,15 +1,17 @@
 variable "vpc_name" {
     description = "Name of VPC"
+    default = "letss"
 }
 
 variable "vpc_cidr" {
     description = "CIDR Block of the VPC"
+    default = "10.0.0.0/24"
 }
 
 variable "eks_cluster_name" {
-    description = "Name of the EKS Cluster"
+    description = "EKSSS"
 }
 
 variable "cidr_block_igw" {
-    description = "CIDR Block for Internet and NAT Gateways "
+    description = " 10.0.0.128/26"
 }
