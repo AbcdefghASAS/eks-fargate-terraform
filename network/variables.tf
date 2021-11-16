d8e6c28a2b09e67f8e19772377f07a30265a6891
Akshaydwagh <49894158+Akshaydwagh@users.noreply.github.com>
2021-11-16 22:18:23 +0530

    Update variables.tf

--- network/variables.tf
+++ network/variables.tf
@@ -13,16 +13,6 @@ variable "eks_cluster_name" {
     default = "EKSS"
 }
 
-variable "cidr_block_igw" {
-    description = "10.0.0.128/26"
-    default = "10.0.0.128/26"
-}
-
-variable "cidr_block_ng" {
-    description = "10.0.0.192/26"
-    default = "10.0.0.192/26"
-}
-
 variable "public_subnets" {
     description = "10.0.0.128/26"
     default = "10.0.0.128/26"
