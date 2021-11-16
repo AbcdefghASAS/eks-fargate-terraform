b6bf1058e29dc9902ed4ffa16939a2a10d9a9210
Akshaydwagh <49894158+Akshaydwagh@users.noreply.github.com>
2021-11-16 21:39:31 +0530

    Update variables.tf

--- network/variables.tf
+++ network/variables.tf
@@ -17,3 +17,8 @@ variable "cidr_block_igw" {
     description = "10.0.0.128/26"
     default = "10.0.0.128/26"
 }
+
+variable "cidr_block_ng" {
+    description = "10.0.0.192/26"
+    default = "10.0.0.192/26"
+}
