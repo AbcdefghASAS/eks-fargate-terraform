f854046b4237c9593233b95172d7d630461994bd
Akshaydwagh <49894158+Akshaydwagh@users.noreply.github.com>
2021-11-17 10:26:25 +0530

    Update variables.tf

--- network/variables.tf
+++ network/variables.tf
@@ -1,24 +1,4 @@
-variable "vpc_name" {
-    description = "Name of VPC"
-    default = "letss"
-}
-
-variable "vpc_cidr" {
-    description = "CIDR Block of the VPC"
-    default = "10.0.0.0/24"
-}
-
-variable "eks_cluster_name" {
-    description = "EKSSS"
-    default = "EKSS"
-}
-
-variable "public_subnets" {
-    description = "10.0.0.128/26"
-    default = "10.0.0.128/26"
-}
-
-variable "private_subnets" {
-    description = "10.0.0.192/26"
-    default = "10.0.0.192/26"
-}
+variable "region" {}
+ variable "main_vpc_cidr" {}
+ variable "public_subnets" {}
+ variable "private_subnets" {}
