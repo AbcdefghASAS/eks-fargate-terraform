e0789c0c49a13a4da10366c39b658376082e6fab
Akshaydwagh <49894158+Akshaydwagh@users.noreply.github.com>
2021-11-17 11:06:23 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,73 +1,26 @@
 variable "region" {
     type = string
     description = "AWS Region"
+    defualt = "us-east-1"
 }
 
 variable "vpc_name" {
     description = "Name of VPC"
+    default = 
 }
 
 variable "vpc_cidr" {
     description = "CIDR Block of the VPC"
+    default = "10.0.0.0/24"
 }
 
 variable "cidr_block_igw" {
     description = "CIDR Block for Internet and NAT Gateways"
+    default = "10.0.0.128/26"
 }
 
 variable "eks_cluster_name" {
     description = "Name of the EKS Cluster"
+    default = "testing"
 }
-
-variable "node_group_name" {
-    type = string
-    description = "Name of the EKS Node Group"
-}
-
-variable "ng_instance_types" {
-    type = list(string)
-    description = "List of instance types associated with the EKS Node Group"
-}
-
-variable "disk_size" {
-    description = "Disk Size for Worker Nodes in GiB"
-}
-
-variable "desired_nodes" {
-    description = "Desired number of worker nodes"
-}
-
-variable "max_nodes" {
-    description = "Maximum number of worker nodes"
-}
-
-variable "min_nodes" {
-    description = "Minimum number of worker nodes"
-}
-
-variable "fargate_profile_name" {
-    type = string
-    description = "Name of the Fargate Profile"
-}
-
-variable "kubernetes_namespace" {
-    type = string
-    description = "Kubernetes namespace for selection"
-}
-
-variable "deployment_name" {
-    type = string
-    description = "Name of the Deployment"
-}
-
-variable "deployment_replicas" {
-    type = string
-    description = "Number of replicas for the Deployment"
-}
-
-variable "app_labels" {
-    type = map
-    description = "List of the labels for Deployment"
-}
-
 
