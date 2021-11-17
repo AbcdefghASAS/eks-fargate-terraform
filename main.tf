0e1ffb00af40a121d2ec851800a8e1c85b7ba9cf
Akshaydwagh <49894158+Akshaydwagh@users.noreply.github.com>
2021-11-17 15:30:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ module "eks_node_group" {
   source            = "./eks/eks_node_group"
   eks_cluster_name  = module.eks_cluster.cluster_name
   node_group_name   = var.node_group_name
-  subnet_ids        = [module.network.aws_subnets_private,module.network.aws_subnets_private]  
+  subnet_ids        = flatten([module.network.aws_subnets_private,module.network.aws_subnets_private])  
   instance_types    = var.ng_instance_types
   disk_size         = var.disk_size
   desired_nodes     = var.desired_nodes
