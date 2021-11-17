fb8697a9b9bc2c0d65b99d87badf845f5492f948
Akshaydwagh <49894158+Akshaydwagh@users.noreply.github.com>
2021-11-17 11:28:13 +0530

    Update variables.tf

--- network/variables.tf
+++ network/variables.tf
@@ -1,7 +1,13 @@
 variable "region" {}
- variable "main_vpc_cidr" {}
- variable "public_subnets" {}
- variable "private_subnets" {}
+ variable "main_vpc_cidr" {
+ default = "10.0.0.0/24"
+ }
+ variable "public_subnets" {
+ default = "10.0.0.128/26"
+ }
+ variable "private_subnets" {
+ default = "10.0.0.192/26"
+ }
 variable "eks_cluster_name" {
     default = "testing"
 }
